118d62dfe4f586b840d03c1cde3c60854cf801a8
Joanna Margareth Nauli <18220007@mahasiswa.itb.ac.id>
2023-02-21 16:37:26 -0500

    Init

---
+++ Week05-Joanna/Week05-Joanna/Week05_JoannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week05_JoannaApp.swift
+//  Week05-Joanna
+//
+//  Created by Joanna Margareth on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Week05_JoannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
